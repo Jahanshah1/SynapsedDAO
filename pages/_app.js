33eb0397b95013b53adc268bbcd3a1a807c9f96b
Jahan Shah <shahjahanrajan@gmail.com>
2024-01-15 16:40:33 +0530

    navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 
-export default MyApp
+
+//INTERNAL IMPORT
+import { VotingProvider } from "../context/voter";
+import NavBar from "../components/NavBar/NavBar";
+
+const MyApp = ({ Component, pageProps }) => (
+  <VotingProvider>
+    <div>
+      <NavBar />
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  </VotingProvider>
+);
+
+export default MyApp;
